8ba973c6d9540552b6ea0ddc69f48a6ded55c337
matzz <matsubaray@eac.ne.jp>
2017-08-10 01:12:42 +0900

    add song

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Botkit = require('botkit');
+const axios = require('axios');
 
 if (!process.env.token) {
     console.log('Error: Specify token in environment');
@@ -11,13 +12,31 @@ const controller = Botkit.slackbot({
 
 controller.spawn({
     token: process.env.token
-}).startRTM(function(err){
+}).startRTM(function (err) {
     if (err) {
         throw new Error(err);
     }
 });
 
 // say hi
-controller.hears('hi',['direct_message','direct_mention','mention'],function(bot,message) {
-    bot.reply(message,'hiho');
+controller.hears(['曲', 'song'], ['direct_message', 'direct_mention', 'mention'], function (bot, message) {
+    console.log(message);
+    const messages = message.text.split(' ');
+    if (messages.length != 2) {
+        bot.reply(message, '使い方が間違っている');
+    }
+    const song = messages[1];
+    const url = 'https://imascg-slstage-wiki.gamerch.com/' + encodeURI(song);
+    console.log(url);
+
+    axios.get(url).then(res => {
+        console.log(res.data);
+        if (res.data.includes(song)) {
+            bot.reply(message, url);
+            return;
+        }
+        throw new Error();
+    }).catch(e => {
+        bot.reply(message, 'そんな曲ないよ');
+    })
 });
